1352ead10e4adf1e525015bf111fe7c3fb91126a
Keith McDuffee <gudlyf@realistek.com>
2018-10-15 14:43:43 -0400

    Added IPv6 rules

--- AWS/userdata.sh
+++ AWS/userdata.sh
@@ -4,6 +4,10 @@ INTERFACE=$(route | grep '^default' | grep -o '[^ ]*$')
 
 echo "Starting Build Process"
 
+echo "Disable SELinux ..."
+
+setenforce 0
+
 echo "Reset DNS settings ..."
 
 echo "supersede domain-name-servers 1.1.1.1, 9.9.9.9;" >> /etc/dhcp/dhclient.conf
@@ -16,7 +20,9 @@ yum -y update
 
 echo "Install packages we need ..."
 
-yum -y install docker
+yum install -y amazon-linux-extras yum-utils device-mapper-persistent-data lvm2
+yum-config-manager --enable extras
+amazon-linux-extras install docker -y
 
 echo "Enable docker ..."
 
@@ -27,17 +33,21 @@ echo "Install and get info for Outline Server ..."
 
 bash -c "$(wget -qO- https://raw.githubusercontent.com/Jigsaw-Code/outline-server/master/src/server_manager/install_scripts/install_server.sh)" > /var/log/outline-install.log
 
-grep "apiUrl" /var/log/outline-install.log > /tmp/outline-install-details.txt
+echo "#####" > /tmp/outline-install-details.txt
+grep "apiUrl" /var/log/outline-install.log >> /tmp/outline-install-details.txt
 export VPN_PORT=$(docker logs shadowbox | grep "tcp server listening" | sed 's/.*0:\(.*\)/\1/')
 export MGMT_PORT=$(grep apiUrl /tmp/outline-install-details.txt | sed -r 's/.*[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}:([0-9]*).*/\1/g')
-echo "" >> /tmp/outline-install-details.txt
+echo "#####" >> /tmp/outline-install-details.txt
 echo -e "\033[0;31mManagement TCP/UDP port number: $MGMT_PORT\033[0m" >> /tmp/outline-install-details.txt
 echo -e "\033[0;33mAccess TCP/UDP port number: $VPN_PORT\033[0m" >> /tmp/outline-install-details.txt
+echo "#####" >> /tmp/outline-install-details.txt
 
-echo "Update security group \"${SECURITY_GROUP}\" to permit VPN Ingress on port $VPN_PORT ..."
-aws --region ${REGION} ec2 authorize-security-group-ingress --group-name ${SECURITY_GROUP} --protocol tcp --port $MGMT_PORT --cidr 0.0.0.0/0
-aws --region ${REGION} ec2 authorize-security-group-ingress --group-name ${SECURITY_GROUP} --protocol udp --port $MGMT_PORT --cidr 0.0.0.0/0
-aws --region ${REGION} ec2 authorize-security-group-ingress --group-name ${SECURITY_GROUP} --protocol tcp --port $VPN_PORT --cidr 0.0.0.0/0
-aws --region ${REGION} ec2 authorize-security-group-ingress --group-name ${SECURITY_GROUP} --protocol udp --port $VPN_PORT --cidr 0.0.0.0/0
+echo "Update security group \"${SECURITY_GROUP}\" to permit VPN Ingress on $MGMT_PORT and $VPN_PORT ..."
+
+aws --region ${REGION} ec2 authorize-security-group-ingress --group-name ${SECURITY_GROUP} --ip-permissions IpProtocol=tcp,FromPort=$MGMT_PORT,ToPort=$MGMT_PORT,IpRanges='[{CidrIp=0.0.0.0/0}]',Ipv6Ranges='[{CidrIpv6=::/0}]'
+aws --region ${REGION} ec2 authorize-security-group-ingress --group-name ${SECURITY_GROUP} --ip-permissions IpProtocol=udp,FromPort=$MGMT_PORT,ToPort=$MGMT_PORT,IpRanges='[{CidrIp=0.0.0.0/0}]',Ipv6Ranges='[{CidrIpv6=::/0}]'
+
+aws --region ${REGION} ec2 authorize-security-group-ingress --group-name ${SECURITY_GROUP} --ip-permissions IpProtocol=tcp,FromPort=$VPN_PORT,ToPort=$VPN_PORT,IpRanges='[{CidrIp=0.0.0.0/0}]',Ipv6Ranges='[{CidrIpv6=::/0}]'
+aws --region ${REGION} ec2 authorize-security-group-ingress --group-name ${SECURITY_GROUP} --ip-permissions IpProtocol=udp,FromPort=$VPN_PORT,ToPort=$VPN_PORT,IpRanges='[{CidrIp=0.0.0.0/0}]',Ipv6Ranges='[{CidrIpv6=::/0}]'
 
 echo "DONE!"
